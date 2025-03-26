5a496119726a599d1d49b55720060e3adf101241
Braden Godley <braden@bgodley.com>
2025-03-26 11:08:49 -0700

    format using gofumpt

--- main.go
+++ main.go
@@ -1,129 +1,128 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"time"
-    "encoding/json"
 )
 
 const ScoreThreshold = 9.0
 
 type CVEResponse struct {
-    TotalResults int `json:"totalResults"`
-    Vulnerabilities []Vulnerability `json:"vulnerabilities"`
+	TotalResults    int             `json:"totalResults"`
+	Vulnerabilities []Vulnerability `json:"vulnerabilities"`
 }
 
 type Vulnerability struct {
-    Cve struct {
-        Id string `json:"id"`
-        Published string `json:"published"`
-        Descriptions []struct {
-            Lang string `json:"lang"`
-            Value string `json:"value"`
-        } `json:"descriptions"`
-        Metrics struct {
-            CvssMetricV31 []struct {
-                CvssData CvssData `json:"cvssData"`
-            } `json:"cvssMetricV31"`
-            CvssMetricV40 []struct {
-                CvssData CvssData `json:"cvssData"`
-            } `json:"cvssMetricV40"`
-            CvssMetricV2 []struct {
-                CvssData CvssData `json:"cvssData"`
-            } `json:"cvssMetricV2"`
-        } `json:"metrics"`
-    } `json:"cve"`
+	Cve struct {
+		Id           string `json:"id"`
+		Published    string `json:"published"`
+		Descriptions []struct {
+			Lang  string `json:"lang"`
+			Value string `json:"value"`
+		} `json:"descriptions"`
+		Metrics struct {
+			CvssMetricV31 []struct {
+				CvssData CvssData `json:"cvssData"`
+			} `json:"cvssMetricV31"`
+			CvssMetricV40 []struct {
+				CvssData CvssData `json:"cvssData"`
+			} `json:"cvssMetricV40"`
+			CvssMetricV2 []struct {
+				CvssData CvssData `json:"cvssData"`
+			} `json:"cvssMetricV2"`
+		} `json:"metrics"`
+	} `json:"cve"`
 }
 
 type CvssData struct {
-    BaseScore float64 `json:"baseScore"`
+	BaseScore float64 `json:"baseScore"`
 }
 
 func main() {
-    log.SetPrefix("coffee: ")
-    log.SetFlags(0)
+	log.SetPrefix("coffee: ")
+	log.SetFlags(0)
 
-    vulns, err := getVulnerabilities()
-    if err != nil {
-        log.Fatal(err)
-    }
+	vulns, err := getVulnerabilities()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    log.Printf("Received %d CVEs", len(vulns.Vulnerabilities))
+	log.Printf("Received %d CVEs", len(vulns.Vulnerabilities))
 
-    totalMatches := 0
+	totalMatches := 0
 
-    for _, v := range vulns.Vulnerabilities {
-        // Skip vulns that don't meet the threshold
-        if score := v.getBaseScore(); score < ScoreThreshold {
-            continue
-        }
+	for _, v := range vulns.Vulnerabilities {
+		// Skip vulns that don't meet the threshold
+		if score := v.getBaseScore(); score < ScoreThreshold {
+			continue
+		}
 
-        log.Printf("Id: %s", v.Cve.Id)
-        for _, desc := range v.Cve.Descriptions {
-            if desc.Lang == "en" {
-                log.Printf("Description: %s", desc.Value)
-            }
-        }
-        for _, data := range v.Cve.Metrics.CvssMetricV31 {
-            log.Printf("Base score: %f", data.CvssData.BaseScore)
-        }
-        log.Println("")
+		log.Printf("Id: %s", v.Cve.Id)
+		for _, desc := range v.Cve.Descriptions {
+			if desc.Lang == "en" {
+				log.Printf("Description: %s", desc.Value)
+			}
+		}
+		for _, data := range v.Cve.Metrics.CvssMetricV31 {
+			log.Printf("Base score: %f", data.CvssData.BaseScore)
+		}
+		log.Println("")
 
-        totalMatches += 1
-    }
+		totalMatches += 1
+	}
 
-    log.Printf("Found %d/%d CVEs with score at least %f", totalMatches, len(vulns.Vulnerabilities), ScoreThreshold)
+	log.Printf("Found %d/%d CVEs with score at least %f", totalMatches, len(vulns.Vulnerabilities), ScoreThreshold)
 }
 
 func getVulnerabilities() (*CVEResponse, error) {
-    end := time.Now()
-    start := end.Add(-24 * time.Hour)
+	end := time.Now()
+	start := end.Add(-24 * time.Hour)
 
-    url := fmt.Sprintf("https://services.nvd.nist.gov/rest/json/cves/2.0/?noRejected&pubStartDate=%v&pubEndDate=%v", start.Format(time.RFC3339), end.Format(time.RFC3339))
+	url := fmt.Sprintf("https://services.nvd.nist.gov/rest/json/cves/2.0/?noRejected&pubStartDate=%v&pubEndDate=%v", start.Format(time.RFC3339), end.Format(time.RFC3339))
 
-    log.Printf("Getting %v", url)
+	log.Printf("Getting %v", url)
 
-    client := &http.Client {
-        Timeout: 10 * time.Second,
-    }
-    resp, err := client.Get(url)
-    if err != nil {
-        return nil, err
-    }
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return nil, err
-    }
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-    var response CVEResponse
-    err = json.Unmarshal(body, &response)
-    if err != nil {
-        return nil, err
-    }
+	var response CVEResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
 
-    return &response, nil
+	return &response, nil
 }
 
 func (v Vulnerability) getBaseScore() float64 {
-    metrics := v.Cve.Metrics
-    if len(metrics.CvssMetricV40) > 0 {
-        return metrics.CvssMetricV40[0].CvssData.BaseScore
-    }
+	metrics := v.Cve.Metrics
+	if len(metrics.CvssMetricV40) > 0 {
+		return metrics.CvssMetricV40[0].CvssData.BaseScore
+	}
 
-    if len(metrics.CvssMetricV31) > 0 {
-        return metrics.CvssMetricV31[0].CvssData.BaseScore
-    }
+	if len(metrics.CvssMetricV31) > 0 {
+		return metrics.CvssMetricV31[0].CvssData.BaseScore
+	}
 
-    if len(metrics.CvssMetricV2) > 0 {
-        return metrics.CvssMetricV2[0].CvssData.BaseScore
-    }
+	if len(metrics.CvssMetricV2) > 0 {
+		return metrics.CvssMetricV2[0].CvssData.BaseScore
+	}
 
-    return 0
+	return 0
 }
-
