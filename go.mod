ffdeac5e49d3ad0880c1488c257a71f3606a25aa
Braden Godley <braden@bgodley.com>
2025-03-30 00:16:48 -0700

    add ai!

--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.22.2
 
 require github.com/samber/lo v1.49.1
 
-require golang.org/x/text v0.21.0 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/sashabaranov/go-openai v1.38.1
+	golang.org/x/text v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"encoding/xml"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +12,8 @@ import (
 	"time"
 
 	"github.com/samber/lo"
+    godotenv "github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 const ScoreThreshold = 9.0
@@ -68,6 +73,7 @@ func main() {
 		}); ok {
 			log.Printf("Description: %s", desc.Value)
 		}
+		v.getCompaniesAffected()
 		log.Println("")
 	}
 
@@ -121,4 +127,74 @@ func (v Vulnerability) getBaseScore() float64 {
 	}
 
 	return 0
+}
+
+func (v Vulnerability) getDescription() (string, error) {
+	if desc, ok := lo.Find(v.Cve.Descriptions, func(desc Description) bool {
+		return desc.Lang == "en"
+	}); ok {
+		return desc.Value, nil
+	} else {
+		return "", errors.New("no description")
+	}
+}
+
+func (v Vulnerability) getCompaniesAffected() ([]string, error) {
+	client := openai.NewClient("")
+
+	desc, err := v.getDescription()
+	if err != nil {
+		return nil, err
+	}
+
+	message := fmt.Sprintf(`I am going to give you a description of a severe CVE vulnerability.
+        Please describe to me which companies you think are most likely affected by this vulnerability, and choose the most important one.
+        We want to send them coffee as a consolation for the stress and effort this new vulnerability is going to place on their engineers.
+        So please also include their physical address so that we can send them this coffee!
+
+        Here is an example response. Please format your response similarly, using XML with the same structure. Don't wrap your response in backticks
+
+        <response>
+            <thinking>
+                Because this vulnerability affects PHP, I think that I should focus on large companies that use PHP. I know of a few companies:
+                - Facebook: Originally built with PHP, Facebook has developed its own version called Hack, which is a derivative of PHP.
+                - Wikipedia: The platform runs on MediaWiki, which is primarily written in PHP.
+                - WordPress: While not a company per se, WordPress powers a significant portion of the web and is built on PHP. Many large organizations use WordPress for their websites.
+                - Slack: The messaging platform uses PHP for some of its backend services.
+                - Tumblr: This microblogging platform is also built using PHP.
+                - Mailchimp: The email marketing service utilizes PHP in its backend.
+                - Yahoo: Parts of Yahoo's services are built using PHP.
+                - Flickr: The photo-sharing platform is another example of a site that uses PHP.
+                Facebook is probably the largest company, so I'll give that as my response.
+            </thinking>
+            <company name="Facebook" address="1 Hacker Way" addressLineTwo="" city="Menlo Park" state="CA" zip="94025"></company>
+        </response>
+
+        Here is the vulnerability description: %s
+    `, desc)
+
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT4oMini,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: message,
+				},
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Response")
+	log.Println(resp.Choices[0].Message.Content)
+
+	return []string{}, nil
+}
+
+func getApiKey() string, error {
+    
 }
